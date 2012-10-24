1ec2d0d65b797afb3562759b04ccbccafb5369ef
Aditya Agarwal <aditya.agarwal@compuware.com>
2012-10-24 16:24:54 -0400

    Updating lisitiners in BaseFormActivity

--- FormValidator/src/com/formvalidator/activities/BaseFormActivity.java
+++ FormValidator/src/com/formvalidator/activities/BaseFormActivity.java
@@ -1,16 +1,47 @@
 package com.formvalidator.activities;
 
-import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import com.formvalidator.R;
 
-public class BaseFormActivity extends Activity {
+public class BaseFormActivity extends BaseActivity implements View.OnClickListener {
     /**
      * Called when the activity is first created.
      */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.form_type_2 );
+        setContentView(R.layout.main );
+
+        Button formOneBtn = (Button)findViewById(R.id.form_1_btn);
+        formOneBtn.setOnClickListener(this);
+
+        Button formTwoBtn = (Button)findViewById(R.id.form_2_btn);
+        formTwoBtn.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        Intent intent = null;
+
+        switch (view.getId()){
+            case R.id.form_1_btn :
+                 intent = new Intent(this, FormTypeOneActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.form_2_btn :
+                intent = new Intent(this, FormTypeTwoActivity.class);
+                startActivity(intent);
+                break;
+
+
+        }
+    }
+
+    @Override
+    public String getTag() {
+        return "BaseFormActivity";
     }
 }
