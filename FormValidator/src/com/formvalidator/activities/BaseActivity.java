4000625067a54b6ef4a93b27b030bb200931b22c
Aditya Agarwal <aditya.agarwal@compuware.com>
2012-10-24 16:22:52 -0400

    Adding abstract BaseActivity
    - Base Activity is implemented by all actvities
    - Logging handled in Base Acivity

--- FormValidator/src/com/formvalidator/activities/BaseActivity.java
+++ FormValidator/src/com/formvalidator/activities/BaseActivity.java
@@ -1,11 +1,82 @@
 package com.formvalidator.activities;
 
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+
 /**
  * Created with IntelliJ IDEA.
- * User: mibaqa0
+ * User: Aditya Agarwal
  * Date: 10/24/12
  * Time: 2:57 PM
- * To change this template use File | Settings | File Templates.
  */
-public class BaseActivity {
+public abstract class BaseActivity extends Activity {
+
+    public abstract String getTag();
+    private static final String MARKER_START = "-_-_-_-_-_-_-_-_-_-_-_";
+    private static final String MARKER_END =   " _-_-_-_-_-_-_-_-_-_-_-";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        mark("onCreate");
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    protected void onPause() {
+        mark("onPause");
+        super.onPause();
+    }
+
+    @Override
+    protected void onStart() {
+        mark("onStart");
+        super.onStart();
+    }
+
+    @Override
+    protected void onStop() {
+        mark("onStop");
+        super.onStop();
+    }
+
+    @Override
+    protected void onRestart() {
+        mark("onRestart");
+        super.onRestart();
+    }
+
+    @Override
+    protected void onResume() {
+        mark("onResume");
+        super.onResume();
+    }
+
+    @Override
+    protected void onPostResume() {
+        mark("onPostResume");
+        super.onPostResume();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle savedInstanceState) {
+        mark("onSaveInstanceState");
+        super.onSaveInstanceState(savedInstanceState);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        mark("onRestoreInstanceState");
+        super.onRestoreInstanceState(savedInstanceState);
+    }
+
+    @Override
+    protected void onDestroy() {
+        mark("onDestroy");
+        super.onDestroy();
+    }
+
+    private void mark(String msg){
+        Log.d(getTag(), MARKER_START + msg + MARKER_END);
+    }
 }
