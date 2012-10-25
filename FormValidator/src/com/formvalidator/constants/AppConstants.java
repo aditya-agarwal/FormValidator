12dbdad40840922916fe2858354fe32551b56c29
Aditya Agarwal <aditya.agarwal@compuware.com>
2012-10-25 12:19:27 -0400

    Updating sdk version

---
+++ FormValidator/src/com/formvalidator/constants/AppConstants.java
@@ -0,0 +1,11 @@
+package com.formvalidator.constants;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: mibaqa0
+ * Date: 10/25/12
+ * Time: 12:10 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class AppConstants {
+}
